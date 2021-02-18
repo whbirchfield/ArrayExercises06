33e59e4aa665393aa6e1d3e457c078e31c973483
Will B <willhbjr@gmail.com>
2021-02-18 06:24:36 +0000

    identified elements in the arrays

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ let element26 = ['iron', 'Fe', 55.85];
 let table = []
 table.push(element1, element2, element26);
 console.log(table);
+console.log("\n");
 
 //c) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
-
-
+console.log(table[1]);
+console.log(table[1][1]);
+console.log("\n");
 
 //d) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
 
-
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
 
 //e) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
